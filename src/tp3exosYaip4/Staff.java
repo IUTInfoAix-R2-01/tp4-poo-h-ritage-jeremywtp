2c8e9c5dac9bb51243fc6361df761c76110c7b8b
watripont <jeremy.watripont@etu.univ-amu.fr>
2025-02-14 12:16:49 +0100

    Commit QCours exo 13 FAIT, Person.java Student.java Staff.java

--- src/tp3exosYaip4/Staff.java
+++ src/tp3exosYaip4/Staff.java
@@ -1,5 +1,40 @@
 package tp3exosYaip4;
 
-public class Staff {
+public class Staff extends Person {
+    private String school;
+    private double pay;
 
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff[" + super.toString() + ",school=" + school + ",pay=" + pay + "€]";
+    }
+
+    public static void main(String[] args) {
+        Staff st = new Staff("Laurence", "QuelquesPartSurTerre", "Iut Aix-Marseille", 1000000);
+        System.out.println(st);
+    }
 }
+
+
--- src/tp3exosYaip4/Student.java
+++ src/tp3exosYaip4/Student.java
@@ -1,5 +1,48 @@
 package tp3exosYaip4;
 
-public class Student {
+public class Student extends Person {
+    private String program;
+    private int year;
+    private double fee;
 
+    public Student(String name, String address, String program, int year, double fee) {
+        super(name, address);
+        this.program = program;
+        this.year = year;
+        this.fee = fee;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public String toString() {
+        return "Student[" + super.toString() + ",program=" + program + ",year=" + year + ",fee=" + fee + "€]";
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("Jeremy", "SurLaLune", "BUT Informatique", 1, 160);
+        System.out.println(s);
+    }
 }
